9758b50e29162d33e775311b02f211f7cc4ea53b
Jonathan C. Lindo, Jr. <jonathanlindo.jr2008@gmail.com>
2025-08-25 16:07:19 +0800

    Add files via upload

---
+++ papyrus/help.py
@@ -0,0 +1,28 @@
+import os, time
+LIGHT_SAND = "\033[38;2;237;201;175m"
+SANDY_BROWN = "\033[38;2;210;180;140m"
+TERRA_COTTA = "\033[38;2;204;102;51m"
+CLAY_BROWN = "\033[38;2;181;101;71m"
+SUNSET_GLOW = "\033[38;2;255;153;102m"
+COYOTE_BROWN = "\033[38;2;125;91;63m"
+RESET = "\033[0m"
+
+def help_info():
+    os.system('cls' if os.name=='nt' else 'clear')
+    print(f"{LIGHT_SAND}Papyrus{RESET} is a tool that helps you find literature, also known as digital {SUNSET_GLOW}“scribes”{RESET} or {SANDY_BROWN}“papyrus”{RESET}, scattered across the vast {CLAY_BROWN}interwebs{RESET}.")
+    print(f"Think of it as your personal librarian, scouring the web for knowledge and bringing it back to you in one place.\n")
+
+    print(f"{TERRA_COTTA}Launch Papyrus.{RESET}")
+    print(f"{COYOTE_BROWN}Enter your query for literature or “scribes.”{RESET}")
+    print(f"{SUNSET_GLOW}Papyrus searches the interwebs for relevant digital scrolls.{RESET}")
+    print(f"{SANDY_BROWN}Browse, save, or export the collected works.{RESET}\n")
+
+    while True:
+        user_input = input(f"{CLAY_BROWN}[e]{SUNSET_GLOW} to{CLAY_BROWN} Exit:{RESET} ").strip().lower()
+        if user_input == 'e':
+            return 'back'
+        else:
+            print(f"{TERRA_COTTA}Invalid input, staying in the current menu.{RESET}")
+
+
+
---
+++ papyrus/papyrus_mainfile.py
@@ -0,0 +1,57 @@
+import webbrowser
+import time
+import os
+from urllib.request import urlretrieve
+import urllib
+import random
+import requests
+
+site_link = "https://oceanofpdf.com/"
+def papyrus_logo():
+
+    shades_of_yellow_ansi = {
+        "bright_yellow" : "\033[38;5;226m {}\033[00m",
+        "orange_yellow" : "\033[38;5;220m {}\033[00m",
+        "banana_yellow" : "\033[38;5;227m {}\033[00m",
+        "pale_yellow" : "\033[38;5;228m {}\033[00m",
+        "very_light_yellow" : "\033[38;5;229m {}\033[00m",
+        "goldenrod" : "\033[38;5;178m {}\033[00m",
+        "saffron" : "\033[38;5;214m {}\033[00m",
+        "mustard" : "\033[38;5;136m {}\033[00m",
+        "lemon_chiffon" : "\033[38;5;230m {}\033[00m",
+        "chartreuse" : "\033[38;5;190m {}\033[00m",
+    }
+    shades_of_yellow_ansi_random = random.choice(list(shades_of_yellow_ansi.values()))
+    
+    def prYellow(s): print((shades_of_yellow_ansi_random).format(s))
+    prYellow("""
+....      ..                                                                           .x+=:.
++^""000h. ~"888h                               ..                                       z`    ^%
+0X.  ?0000X  8888f               .d``          @L             .u    .      x.    .          .   <k
+'008x  8888X  8888~        u      @8Ne.   .u   9888i   .dL   .d88B :@8c   .@88k  z88u      .@8Ned8"
+'88888 8888X   "88x:    us888u.   %8888:u@88N  `Y888k:*888. ="8888f8888r ~"8888 ^8888    .@^%8888"
+ `8888 8888X  X88x.  .@88 "8888"   `888I  888.   888E  888I   4888>'88"    8888  888R   x88:  `)8b.
+   `*` 8888X '88888X 9888  9888     888I  888I   888E  888I   4888> '      8888  888R   8888N=*8888
+  ~`...8888X  "88888 9888  9888     888I  888I   888E  888I   4888>        8888  888R    %8"    R88
+   x8888888X.   `%8" 9888  9888   uW888L  888'   888E  888I  .d888L .+     8888 ,888B .   @8Wou 9%
+  '%"*8888888h.   "  9888  9888  '*88888Nu88P   x888N><888'  ^"8888*"     "8888Y 8888"  .888888P`
+  ~    888888888!`   "888*""888" ~ '88888F`      "88"  888      "Y"        `Y"   'YP    `   ^"F
+       X888^""      ^Y"   ^Y'     888 ^              88F
+       `88f                         *8E               98"
+        88                          '8>             ./"
+        ""                           "             ~`
+""")
+# papyrus_logo()
+
+def scribere():
+    time.sleep(1)
+    print("\033[38;2;255;127;80m➤ Book Name: \033[0m", end='')
+    searched_book_name = input(' ')
+# scribere()
+
+def main():
+    os.system('cls' if os.name=='nt' else 'clear')
+    papyrus_logo()
+    time.sleep(0.5)
+    print('\n')
+    scribere()
