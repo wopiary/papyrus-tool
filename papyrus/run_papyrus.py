e3f89ea2310278736b2b930f7add32b7cb758b07
Jonathan C. Lindo, Jr. <jonathanlindo.jr2008@gmail.com>
2025-09-01 18:31:08 +0800

    Update run_papyrus.py
    this is the current version i have

--- papyrus/run_papyrus.py
+++ papyrus/run_papyrus.py
@@ -4,28 +4,48 @@ import os
 import random
 import logo_animation as module_animate
 from curses import wrapper
+import papyrus_mainfile
+import help
 
 def main():
+    os.system('cls' if os.name=='nt' else'clear')
     module_animate.wave_art(delay=0.05)
-    def prGold(s): print("\033[38;5;220m {}\033[00m".format(s)) 
-    def prTurquoise(s): print("\033[38;5;44m {}\033[00m".format(s)) 
-    def prClayRed(s): print("\033[38;5;160m {}\033[00m".format(s))  
-    def prVeryLightYellow(s): print("\033[38;5;229m {}\033[00m".format(s))
-    def prCoyoteBrown(s): print("\033[38;2;125;91;63m {}\033[0m".format(s))   
 
-    prVeryLightYellow("\nLiber scriptum est. Et scientia? Sit libera. ")       
-    prGold("\n [1] Start Program")
-    prTurquoise("[2] Settings")
-    prClayRed("[3] Exit Program")
-    print("\n\033[38;2;125;91;63m➤ Select Option: \033[0m", end='')
-    user_input = input()
+    gold = "\033[38;5;220m"
+    very_light_yellow = "\033[38;5;229m"
+    turquoise = "\033[38;5;44m"
+    clay_red = "\033[38;5;160m"
+    coyote_brown = "\033[38;2;125;91;63m"
+    cactus_green = "\033[38;2;107;142;35m"
+    golden_dune = "\033[38;2;218;165;105m"
+    sahara_beige = "\033[38;2;244;196;153m"
+    reset_color = "\033[0m"
+
+    
+    print(f"\n\n{very_light_yellow}Liber scriptum est. Et scientia? Sit libera{reset_color}")
+    print(f"\n{sahara_beige}==========================================={reset_color}")
+    print(f"{gold}[1] Start Program{reset_color}")
+    print(f"{turquoise}[2] Settings{reset_color}")
+    print(f"{clay_red}[3] ReadME{reset_color}")
+    print(f"{coyote_brown}[4] Exit Program{reset_color}")
+    print(f"{sahara_beige}==========================================={reset_color}")
+    user_input = input(f"\n{cactus_green}➤ Select Option{reset_color} ")
+    if len(user_input.strip()) == 0:
+        main()
+
     match user_input:
         case '1':
-            print('unrolling...')
+            result = papyrus_mainfile.main()
         case '2':
+            pass
+        case '3':
+            result = help.help_info()
+            if result =='back':
+                main()
+        case '4':
+            print(f"{golden_dune}Exiting...{reset_color}")
             exit()
-        case _:
-            main()
+
+            
     
 main()
-
